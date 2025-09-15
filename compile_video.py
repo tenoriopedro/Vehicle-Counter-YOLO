b36904f54149a1fd10901e6837eba22e90e016f9
tenoriopedro <pstsouza13@gmail.com>
2025-09-15 23:16:31 +0100

    Add flake8 to the project

--- compile_video.py
+++ compile_video.py
@@ -1,5 +1,5 @@
-## If necessary, adjust torch and torchvision versions
-## torch==2.3.1 torchvision==0.18.1
+# If necessary, adjust torch and torchvision versions
+# torch==2.3.1 torchvision==0.18.1
 import cv2
 import os
 from pathlib import Path
@@ -16,7 +16,14 @@ ROOT_FILE = ROOT / "test_files/track_video_car01.mp4"
 model = YOLO("YOLOweights\\yolov8x.pt")
 cap = cv2.VideoCapture(str(ROOT_FILE))
 
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+w, h, fps = (
+    int(cap.get(x))
+    for x in (
+        cv2.CAP_PROP_FRAME_WIDTH,
+        cv2.CAP_PROP_FRAME_HEIGHT,
+        cv2.CAP_PROP_FPS
+    )
+)
 
 # Classes to be detected (COCO IDs)
 class_to_count = [2, 3, 7]  # (2: car, 3: motorcycle, 7: truck)
@@ -25,7 +32,12 @@ class_names = {2: "Car", 3: "Motorcycle", 7: "Truck"}
 # Line coordinates for tracking
 line_points = [(20, 400), (1500, 400)]
 
-video_writer = cv2.VideoWriter("compiled_files\\object_counting_output.avi", cv2.VideoWriter_fourcc(*"XVID"), fps, (w, h))
+video_writer = cv2.VideoWriter(
+    "compiled_files\\object_counting_output.avi",
+    cv2.VideoWriter_fourcc(*"XVID"),
+    fps,
+    (w, h)
+)
 
 # Initialize Object Counter
 counter = object_counter.ObjectCounter()
@@ -47,13 +59,16 @@ counts = {
 while cap.isOpened():
     success, im0 = cap.read()
     if not success:
-        print("Video frame is empty or video processing has been successfully completed.")
+        print(
+            "Video frame is empty"
+            "or video processing has been successfully completed."
+        )
         break
-    
+
     tracks = model.track(
-        im0, 
-        persist=True, 
-        show=False, 
+        im0,
+        persist=True,
+        show=False,
         classes=class_to_count
     )
     im0 = counter.start_counting(im0, tracks)
@@ -61,7 +76,7 @@ while cap.isOpened():
     # Get the total number of vehicles passing through
     counter_vehicles = counter.in_counts + counter.out_counts
     total_vehicles = counter_vehicles
-    
+
     # Update counts per class and direction based on ObjectCounter output
     for class_name, direction_counts in counter.class_wise_count.items():
         # class_name is the class label (e.g., 'car')
@@ -71,7 +86,7 @@ while cap.isOpened():
             # Map model class names to class IDs
 
             if class_name == model_class_name:
-                
+
                 for direction, count in direction_counts.items():
 
                     if direction == 'IN':
@@ -85,16 +100,18 @@ while cap.isOpened():
     cv2.addWeighted(overlay, 0.6, im0, 0.4, 0, im0)
 
     # Add title
-    cv2.putText(im0, "Traffic Counter", (20, 50), 
+    cv2.putText(im0, "Traffic Counter", (20, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     # Add class-specific counts
     y_offset = 90
     for cls_id in class_to_count:
 
-        text = (f"{class_names[cls_id]} "
-        f"( Left: {counts['left'][cls_id]} | "
-        f" Right: {counts['right'][cls_id]} )")
+        text = (
+            f"{class_names[cls_id]} "
+            f"( Left: {counts['left'][cls_id]} | "
+            f" Right: {counts['right'][cls_id]} )"
+        )
 
         cv2.putText(im0, text, (20, y_offset),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
@@ -103,8 +120,11 @@ while cap.isOpened():
 
     # Display total vehicle count on screen
     total_vehicles_text = f"Total vehicles: {total_vehicles}"
-    cv2.putText(im0, total_vehicles_text, (20, y_offset),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    cv2.putText(
+        im0, total_vehicles_text,
+        (20, y_offset), cv2.FONT_HERSHEY_SIMPLEX,
+        0.7, (0, 255, 0), 2
+    )
 
     video_writer.write(im0)
 
--- show_results.py
+++ show_results.py
@@ -1,6 +1,5 @@
 from IPython.display import HTML
 from base64 import b64encode
-import os
 import moviepy.editor as moviepy
 from pathlib import Path
 
@@ -12,14 +11,15 @@ VIDEO_RESULT = ROOT / "result_files"
 
 def show_video(path_video):
 
-  mp4 = open(path_video, "rb").read()
-  data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+    mp4 = open(path_video, "rb").read()
+    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
 
-  return HTML("""
-  <video width=500 controls>
-    <source src='%s' type='video/mp4'>
-    </video>
-    """ % data_url)
+    return HTML("""
+    <video width=500 controls>
+      <source src='%s' type='video/mp4'>
+      </video>
+      """ % data_url)
+
 
 save_video = str(COMPILED_FILES)
 final_video = str(VIDEO_RESULT / "video_countingCar_result01.mp4")
