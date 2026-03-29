2e5a7de4b74b17366c57fdb3f4011e2e435adaac
Pedro Tenorio <pstsouza13@gmail.com>
2026-03-29 23:44:35 +0100

    refactor: encapsulate couting logic in the TrafficCounter class

--- src/vehicle_counter/compile_video.py
+++ src/vehicle_counter/compile_video.py
@@ -1,9 +1,8 @@
 import time
-from pathlib import Pathsit
+from pathlib import Path
 
 import cv2
 from ultralytics import YOLO
-from ultralytics.solutions import ObjectCounter
 
 
 class TrafficCounter:
@@ -12,11 +11,15 @@ class TrafficCounter:
             self,
             model_path: Path,
             video_source: Path,
-            output_dir: Path) -> None:
+            output_dir: Path,
+            classes_to_count: list[int],
+            line_points: list[tuple[int, int]]) -> None:
 
         self.model_path = model_path
         self.video_source = video_source
         self.output_dir = output_dir
+        self.class_to_count = classes_to_count
+        self.line_points = line_points
 
         if not self.model_path.exists():
             msg = "Modelo não encontrado"
@@ -31,45 +34,34 @@ class TrafficCounter:
         # Initialize Model
         self.model = YOLO(self.model_path)
 
-        # Classes to be detected (COCO IDs)
-        # 2= Car ; 3= Motorcycle ; 5= Bus ; 7= Truck
-        self.class_to_count = [2, 3, 5, 7]
+        self.detected_data = {
+            "timestamp": [],
+            "track_id": [],
+            "class_id": [],
+            "confidence": [],
+            "direction": [],
+        }
 
-        # Line coordinates for tracking
-        self.line_points = [(20, 400), (1500, 400)]
+        self.processed_ids = set()
 
-        """# Initialize Object Counter
-        self.counter = ObjectCounter(
-            region=self.line_points,
-            model=self.model,
-            show_in=True,
-            show_out=True,
-            classes=self.class_to_count
 
-        )"""
 
 
     def run(self, file_name: str = "object_output_result") -> None:
 
         cap = cv2.VideoCapture(str(self.video_source))
 
-        width, height, fps = (
-            int(cap.get(x))
-            for x in (
-                cv2.CAP_PROP_FRAME_WIDTH,
-                cv2.CAP_PROP_FRAME_HEIGHT,
-                cv2.CAP_PROP_FPS
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        if fps <= 0:
+            msg = (
+                "Falha ao extrair metadados do video. "
+                f"Obtido: FPS={fps}\n"
+                "Verifique a integridade da fonte de vídeo"
             )
-        )
-
-        output_result = (self.output_dir / file_name).with_suffix(".mp4")
-
-        video_writer = cv2.VideoWriter(
-            str(output_result),
-            cv2.VideoWriter_fourcc(*"mp4v"),  # type: ignore
-            fps,
-            (width, height)
-        )
+            raise ValueError(
+                msg
+            )
 
         try:
 
@@ -83,38 +75,16 @@ class TrafficCounter:
                     break
 
                 results = self.model.track(
-                    frame,
+                    source=frame,
                     persist=True,
                     classes=self.class_to_count
                 )
 
 
         finally:
-            cap.release()
-            video_writer.release()
-            cv2.destroyAllWindows()
+            ...
 
-
-def run() -> None:
-    start = time.time()
-
-    # Root Project
-    root = Path(__file__).parent.parent.parent
-
-    model_path = root / "models/yolov8m.pt"
-    video_file = root / "data/raw/track_video_vehicles.mp4"
-    output_dir = root / "data/processed"
-
-    traffic = TrafficCounter(model_path, video_file, output_dir)
-
-    file_name_result = "video_result"
-    traffic.run(file_name_result)
-
-    end = time.time()
-    total_time = end - start
-
-    print(f"Inference Time: {total_time:.1f} seconds")
 
 
 if __name__ == "__main__":
-    run()
+    ...
